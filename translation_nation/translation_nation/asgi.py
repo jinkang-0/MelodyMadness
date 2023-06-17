895f2ad529a62bee22c9696732069d5e2a09918d
ImaMoonky <Trickstabber101@gmail.com>
2023-06-17 11:55:12 -0700

    Added Django Backend

---
+++ translation_nation/translation_nation/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for translation_nation project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "translation_nation.settings")
+
+application = get_asgi_application()
---
+++ translation_nation/translation_nation/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for translation_nation project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "translation_nation.settings")
+
+application = get_wsgi_application()

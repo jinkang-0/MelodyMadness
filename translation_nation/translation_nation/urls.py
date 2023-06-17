8ed3492a3047a9f39d9c4d2c2db42aaf0ebfbb98
ImaMoonky <Trickstabber101@gmail.com>
2023-06-17 12:41:31 -0700

    Added Home Page As Chat Interface

--- translation_nation/translation_nation/urls.py
+++ translation_nation/translation_nation/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("chat_interface/", include('chat_interface.urls')),
+    path("", include('chat_interface.urls')),
 ]

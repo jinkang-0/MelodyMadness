786fd75d4a48d66d2277e9bdc52ed1c697acdac8
ImaMoonky <Trickstabber101@gmail.com>
2023-06-17 13:00:10 -0700

    Created Universal Base Template for Application

--- translation_nation/chat_interface/views.py
+++ translation_nation/chat_interface/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Handle Traffic From Home Page of Chat Application
 def home(request):
-    return HttpResponse("<h1> Blog Home </h1>")
+    return render(request, "chat_interface/chat_home.html")
--- translation_nation/translation_nation/settings.py
+++ translation_nation/translation_nation/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "chat_interface.apps.ChatInterfaceConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",

64dae424337e37da3c77509546f53421ecf52c6e
jinkang-0 <jinkang.fang@gmail.com>
2023-06-18 08:32:39 -0700

    connected backend route with melody

--- core/chat_interface/views.py
+++ core/chat_interface/views.py
@@ -1,8 +1,11 @@
 from django.views.decorators.csrf import csrf_exempt
-from django.http import HttpResponse
+from django.http import JsonResponse
 from django.shortcuts import render
 from scripts import midi
 from os import path
+import base64
+from scipy.io import wavfile
+import io
 
 def front(request):
     context = {}
@@ -14,34 +17,28 @@ def melody(request):
 
     # handle post request
     if request.method == "POST":
-        return HttpResponse("test:helloworld")
-        # return render(request, 'index.html', { "data": data })
-            # # Chat GPT magic
-            # melody = [
-            #     (60, 1), # C4 (middle C), quarter note
-            #     (62, 1), # D4, quarter note
-            #     (64, 1), # E4, quarter note
-            #     (65, 1), # F4, quarter note
-            #     (67, 1), # G4, quarter note
-            #     (69, 1), # A4, quarter note
-            #     (71, 1), # B4, quarter note
-            #     (72, 2), # C5 (1 octave above middle C), half note
-            #     (71, 1), # B4, quarter note
-            #     (69, 1), # A4, quarter note
-            #     (67, 1), # G4, quarter note
-            #     (65, 1), # F4, quarter note
-            #     (64, 1), # E4, quarter note
-            #     (62, 1), # D4, quarter note
-            #     (60, 2), # C4, half note
-            #     (72, 1), # C5, quarter note
-            #     (74, 1), # D5, quarter note
-            #     (76, 1), # E5, quarter note
-            #     (77, 1), # F5, quarter note
-            #     (79, 1)  # G5, quarter note
-            # ]
+        #  Replace with chat GPT output
+        melody = [
+            (60, 1), (62, 1), (64, 1), (65, 1), (67, 1), (69, 1), (71, 1), (72, 2),(71, 1), (69, 1), (67, 1), (65, 1), (64, 1), (62, 1), (60, 2), (72, 1), (74, 1), (76, 1), (77, 1), (79, 1)  
+        ]
 
-            # midi.export_midi(midi.generate_midi(480, melody, 0), 100, 'output.wav')
-            # return render(request, "index.html", { "audioFile": path.realpath('output.wav') })
+        mid = midi.generate_midi(480, melody, 0)
+        midi.export_midi(mid, 100, 'output.wav')
+        
+        # send file
+        # @source https://gist.github.com/hadware/8882b980907901426266cb07bfbfcd20
+        with open('output.wav', 'rb') as f:
+            input_wav = f.read()
+
+        rate, data = wavfile.read(io.BytesIO(input_wav))
+        reversed_data = data[::-1]
+        bytes_io = io.BytesIO(bytes())
+        wavfile.write(bytes_io, rate, reversed_data)
+
+        output_wav = bytes_io.read()
+        encoded_string = base64.b64encode(output_wav).decode("ascii")
+
+        return JsonResponse({ "audioFile": encoded_string })
 
     return render(request, "index.html", context)
 
